9065bdf3b12f10e85363de0aed572e6c1d443fa8
myxamediyar <mukhamediyar.k@gmail.com>
2023-02-19 16:29:29 -0800

    some cs88 q

--- someFunc.py
+++ someFunc.py
@@ -74,7 +74,6 @@ def werewolves():
     # 0 - w, 1 - v
     players = [1,1,0,1,1,0,1,0,0,1,0,0,1,1,0,1]
 
-
     def find(li):
         if len(li) == 1:
             return (li[0], li[0])
@@ -187,5 +186,45 @@ def num_to_b(x):
     res_10p = num_to_b(str(10 ** (len(x) // 2))) #left shift O(n)
     resl = bin(int(resl, 2) * int(res_10p, 2)) #karatsuba O(n^1.6)
     return bin(int(resl, 2) + int(resr, 2))
+#print(num_to_b('420'))
 
-print(num_to_b('420'))
+#garbo algo ezz
+def cycle(f1, f2, f3):
+    def helpern(n):
+        def helperx(x):
+            a = n
+            for i in range(a // 3):
+                x = f3(f2(f1(x)))
+            a = a % 3
+            if a == 1:
+                return f1(x)
+            if a == 2:
+                return f2(f1(x))
+            return x
+        return helperx
+    return helpern
+
+def add1(x):
+        return x + 1
+def times2(x):
+        return x * 2
+def add3(x):
+    return x + 3
+my_cycle = cycle(add1, times2, add3)
+identity = my_cycle(0)
+print(identity(5))
+# 5
+add_one_then_double = my_cycle(2)
+print(add_one_then_double(1))
+# 4
+do_all_functions = my_cycle(3)
+print(do_all_functions(2))
+#9
+do_more_than_a_cycle = my_cycle(4)
+print(do_more_than_a_cycle(2))
+#10
+do_two_cycles = my_cycle(6)
+print(do_two_cycles(1))
+#19
+            
+            
