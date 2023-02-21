80b7caaf522efbbb0ec8bb002637a7cc715413ce
myxamediyar <mukhamediyar.k@gmail.com>
2023-02-20 17:33:32 -0800

    implemented some EZZZZ algorithm but decided its tooo EZZZZ so deleted

--- someFunc.py
+++ someFunc.py
@@ -213,19 +213,19 @@ def add3(x):
     return x + 3
 my_cycle = cycle(add1, times2, add3)
 identity = my_cycle(0)
-print(identity(5))
-# 5
-add_one_then_double = my_cycle(2)
-print(add_one_then_double(1))
-# 4
-do_all_functions = my_cycle(3)
-print(do_all_functions(2))
-#9
-do_more_than_a_cycle = my_cycle(4)
-print(do_more_than_a_cycle(2))
-#10
-do_two_cycles = my_cycle(6)
-print(do_two_cycles(1))
+# print(identity(5))
+# # 5
+# add_one_then_double = my_cycle(2)
+# print(add_one_then_double(1))
+# # 4
+# do_all_functions = my_cycle(3)
+# print(do_all_functions(2))
+# #9
+# do_more_than_a_cycle = my_cycle(4)
+# print(do_more_than_a_cycle(2))
+# #10
+# do_two_cycles = my_cycle(6)
+# print(do_two_cycles(1))
 #19
 #endregion test-cases      
             
