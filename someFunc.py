f2bdb7f9d4c24c020dba4c46887160cadcbdbd86
myxamediyar <mukhamediyar.k@gmail.com>
2023-03-12 03:14:10 -0700

    DP problems

--- someFunc.py
+++ someFunc.py
@@ -1,3 +1,4 @@
+#region link
 class Link:
     def __init__(self, value, rest=None) -> None:
         self.value = value
@@ -13,7 +14,9 @@ def combineLink(link, fn):
 
 # print(combineLink(l1, lambda x,y: x + y))
 # print(0b111 >> 1)
+#endregion link
 
+#region grabcode
 import itertools
 import numpy as np
 def nik():
@@ -67,7 +70,9 @@ def mything():
     sol2 = sum(i*(a-b) for i,(a,b) in enumerate(zip(sorted(arr2),sorted(arr1,reverse=True))))
     print(sol2, curmax)
     return sol2 == curmax
+#endregion grabcode
 
+#region werewolves
 import random
 def werewolves():
     lie = (lambda : random.randint(0,1))
@@ -109,6 +114,9 @@ def werewolves():
 
 # print(werewolves())
 
+#endregion werewolves
+
+#region binsrch
 def binary_search_recursive(lst, of):
     #print(of)
     if len(lst) == 0:
@@ -131,11 +139,9 @@ def binary_search_recursive(lst, of):
 # a = [1,2,3,4,5,6,7,8,9]
 # print(binary_search_recursive(a, 9))
 
-def FFT(poly):
-    odd = poly[1::2]
-    even = poly[0::2]
-    res = []
+#endregion binsrch
     
+#region subarraysum
 import math
 def subarray_sum(li):
 
@@ -171,8 +177,9 @@ def subarray_sum(li):
     return sum(li[res[0]:res[1] + 1])
 
 #print(subarray_sum([-2,1,-3,4,-1,2,1,-5,4]))
+#endregion subarraysum
 
-
+#region num_to_bin
 def num_to_b(x):
     if int(x) <= 10:
         return bin(int(x))
@@ -187,6 +194,8 @@ def num_to_b(x):
     resl = bin(int(resl, 2) * int(res_10p, 2)) #karatsuba O(n^1.6)
     return bin(int(resl, 2) + int(resr, 2))
 #print(num_to_b('420'))
+#endregion num_to_bin
+
 
 #region test-cases
 #garbo algo ezz
@@ -228,4 +237,154 @@ identity = my_cycle(0)
 # print(do_two_cycles(1))
 #19
 #endregion test-cases      
-            
+
+#region generator
+def hailstone(n):
+    """Yields the elements of the hailstone sequence starting at n.
+       At the end of the sequence, yield 1 infinitely.
+
+    >>> hail_gen = hailstone(10)
+    >>> [next(hail_gen) for _ in range(10)]
+    [10, 5, 16, 8, 4, 2, 1, 1, 1, 1]
+    >>> next(hail_gen)
+    1
+    """
+    # yield n
+    #print(n)
+    if n == 1:
+        yield 1
+            #return
+    elif n % 2 == 0:
+        yield from [num for num in hailstone(n // 2)]
+        #yield from hailstone(n // 2)
+    else:
+        yield from [num for num in hailstone(n * 3 + 1)]
+        #yield from hailstone(n * 3 + 1)
+
+hail_gen = hailstone(10)
+# print(next(hail_gen))
+# print(next(hail_gen))
+# print(next(hail_gen))
+# print(next(hail_gen))
+a = [1,2,32,3,4,5]
+def b(a):
+    print(a, "blya")
+    if a == 1:
+        yield -9000
+        return
+
+    # print(a, 2)
+    g = b(a - 1)
+    def h(i):
+        print(i)
+        return i
+    yield from [h(i) for i in g]
+c = b(10)
+#print("RETURNED", next(c))
+# print(next(c))
+
+r = [1,2,3,4,4,56]
+def l(r):
+    counter = 0
+    while counter < 10:
+        print("aaa")
+        yield -1
+        counter += 1
+    yield from r
+def p(r):
+    yield from [h for h in l(r)]
+
+gen1 = l(r)
+gen2 = p(r)
+
+def perms(seq):
+    """Generates all permutations of the given sequence. Each permutation is a
+    list of the elements in SEQ in a different order. The permutations may be
+    yielded in any order.
+
+    >>> p = perms([100])
+    >>> type(p)
+    <class 'generator'>
+    >>> next(p)
+    [100]
+    >>> try: #this piece of code prints "No more permutations!" if calling next would cause an error
+    ...     next(p)
+    ... except StopIteration:
+    ...     print('No more permutations!')
+    No more permutations!
+    >>> sorted(perms([1, 2, 3])) # Returns a sorted list containing elements of the generator
+    [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+    >>> sorted(perms((10, 20, 30)))
+    [[10, 20, 30], [10, 30, 20], [20, 10, 30], [20, 30, 10], [30, 10, 20], [30, 20, 10]]
+    >>> sorted(perms("ab"))
+    [['a', 'b'], ['b', 'a']]
+    """
+    if not seq:
+        yield []
+    else:
+        for sub_perm in perms(seq[1:]):
+            for i in range(len(seq)):
+                #yield p[:i] + [seq[0]] + p[i:]
+                print(seq, sub_perm)
+                sub_perm.insert(i, seq[0])
+                lol = sub_perm.copy()
+                sub_perm.pop(i)
+                yield lol
+                # yield sub_perm[:i] + [seq[0]] + sub_perm[i:]
+
+c = perms([100])
+# print(type(c))
+# print(sorted(perms([1, 2, 3]))) # Returns a sorted list containing elements of the generator
+# print(sorted(perms((10, 20, 30))))
+# print(sorted(perms("ab")))
+#endregion generator
+
+
+#region nlenpipe
+
+n = list(range(1, 9))
+pr = [1,5,8, 9,10,17,17,20]
+
+def getprice(N, n, pr):
+    f = [0] * (N + 1)
+    f[0] = 0
+    for i in range(1, N+1):
+        cand = [j for j in n if j <= i]
+        f[i] = max([f[i - cand[j]] + pr[j] for j in range(len(cand))])
+    return f[N], f
+
+#print(getprice(8, n, pr))
+#endregion nlenpipe
+
+#region striginterpretation
+def substr(x, y):
+    if len(y) > len(x):
+        return None
+    x1 = x
+    if len(x) > len(y):
+        x1 = x[len(x) - 1: len(x) - len(y) - 1: -1][::-1]
+    for i in range(len(x1)):
+        if x1[i] != y[i]:
+            return None
+    return x[len(x) - len(y) - 1::-1][::-1] if len(x) != len(y) else ""
+
+s = "101101"
+d = {"a": "1", "b": "01", "c": "101"}
+
+def countInterpretation(s, d):
+    memo = {}
+    def f(s, d):
+        if s == None:
+            return 0
+        if s == "":
+            return 1
+        if s in memo:
+            return memo[s]
+        count = 0
+        for w in list(d.keys()):
+            count += f(substr(s, d[w]), d)
+        return count
+    return f(s, d)
+
+print(countInterpretation(s, d))
+#endregion striginterpretation
